e42bf0dd1280c9bafef9b378a3341d77c244fc28
Mohamed <md.diag75@gmail.com>
2026-05-10 18:45:31 +0100

    feat: update root response

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "sample-service ok")
+		fmt.Fprintln(w, "sample-service ok v2")
 	})
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
